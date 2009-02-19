e2af432817295371c9cdb1bc0b020738852c6d5b
f.r.u.x.o.j <f.r.u.x.o.j@6e11d110-fbaa-11dd-87c9-111d11f6bf44>
2009-02-19 16:19:37 +0000

    Basic method renaming, comments and first impl. of the Chunk class.

--- trunk/src/com/google/code/peersim/starstream/protocol/Chunk.java
+++ trunk/src/com/google/code/peersim/starstream/protocol/Chunk.java
@@ -5,20 +5,25 @@
 
 package com.google.code.peersim.starstream.protocol;
 
+import com.google.code.peersim.pastry.protocol.PastryId;
+import com.google.code.peersim.pastry.protocol.PastryResource;
+
 /**
+ * This class is used to represent chunks that must be disseminated to every node
+ * during the streaming event. Being a {@link PastryResource}, it is uniquely
+ * identified by a {@link PastryId} instance.
  *
  * @author frusso
- * @version
- * @since
+ * @version 0.1
+ * @since 0.1
  */
-public class Chunk {
-
-  public static class ChunkId {
-
+public class Chunk<T> extends PastryResource<T> {
+  
+  /**
+   * Constructor.
+   * @param chunk The actual content
+   */
+  public Chunk(T chunk) {
+    super(chunk);
   }
-
-  public ChunkId getId() {
-    throw new UnsupportedOperationException("Not yet implemented");
-  }
-
 }
