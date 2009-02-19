406cfc7675de13e0e8f393eb35b6b45ae07cf718
f.r.u.x.o.j <f.r.u.x.o.j@6e11d110-fbaa-11dd-87c9-111d11f6bf44>
2009-02-19 16:19:11 +0000

    Basic method renaming, comments and first impl. of the Chunk class.

--- trunk/src/com/google/code/peersim/starstream/protocol/messages/ChunkKo.java
+++ trunk/src/com/google/code/peersim/starstream/protocol/messages/ChunkKo.java
@@ -1,10 +1,15 @@
 package com.google.code.peersim.starstream.protocol.messages;
 
-import com.google.code.peersim.starstream.protocol.Chunk.ChunkId;
+import com.google.code.peersim.pastry.protocol.PastryId;
 import com.google.code.peersim.starstream.protocol.StarStreamNode;
 import com.google.code.peersim.starstream.protocol.messages.StarStreamMessage.Type;
 
 /**
+ * This message is used to notify the sender of a {@link ChunkMessage} message that
+ * the chunk has either not been received (i.e. due to timeout expiration) or
+ * received currupted. The message must carry the unique identifier of the chunk
+ * it refers to.
+ *
  * @author frusso
  * @version 0.1
  * @since 0.1
@@ -12,12 +17,14 @@ import com.google.code.peersim.starstream.protocol.messages.StarStreamMessage.Ty
 public class ChunkKo extends ChunkAdvertisement {
 
   /**
+   * Constructor. When creating a new instance, the specified source is also used to
+   * initialize the message originator.
    * 
-   * @param src
-   * @param dst
-   * @param chunkId
+   * @param src The sender
+   * @param dst The destination
+   * @param chunkId The identifier of the chunk that has not been properly received
    */
-  ChunkKo(StarStreamNode src, StarStreamNode dst, ChunkId chunkId) {
+  ChunkKo(StarStreamNode src, StarStreamNode dst, PastryId chunkId) {
     super(src, dst, chunkId);
   }
 
