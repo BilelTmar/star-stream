ea1dc8d73dc9ea65effce32961da7042116ff917
f.r.u.x.o.j <f.r.u.x.o.j@6e11d110-fbaa-11dd-87c9-111d11f6bf44>
2009-02-19 16:19:24 +0000

    Basic method renaming, comments and first impl. of the Chunk class.

--- trunk/src/com/google/code/peersim/starstream/protocol/messages/ChunkAdvertisement.java
+++ trunk/src/com/google/code/peersim/starstream/protocol/messages/ChunkAdvertisement.java
@@ -1,9 +1,14 @@
 package com.google.code.peersim.starstream.protocol.messages;
 
-import com.google.code.peersim.starstream.protocol.Chunk.ChunkId;
+import com.google.code.peersim.pastry.protocol.PastryId;
 import com.google.code.peersim.starstream.protocol.StarStreamNode;
 
 /**
+ * This message is used to advertise the availability of a new chunk at the
+ * (at least) advertising node. This message simply carries the chunk unique
+ * identifier. Interested nodes have to reply this message with the appropriate
+ * {@link ChunkRequest} to obtain the advertised chunk.
+ *
  * @author frusso
  * @version 0.1
  * @since 0.1
@@ -11,34 +16,30 @@ import com.google.code.peersim.starstream.protocol.StarStreamNode;
 public class ChunkAdvertisement extends StarStreamMessage {
 
   /**
-   * 
+   * The advertised chunk's unique identifier.
    */
-  private ChunkId chunkId;
+  private final PastryId chunkId;
 
   /**
+   * Constructor. When creating a new instance, the specified source is also used to
+   * initialize the message originator.
    *
-   * @param src
-   * @param dst
-   * @param chunkId
+   * @param src The sender
+   * @param dst The destination
+   * @param chunkId The advertised chunk unique identifier
    */
-  ChunkAdvertisement(StarStreamNode src, StarStreamNode dst, ChunkId chunkId) {
+  ChunkAdvertisement(StarStreamNode src, StarStreamNode dst, PastryId chunkId) {
     super(src, dst);
+    if(chunkId==null) throw new IllegalArgumentException("The chunk identifier cannot be 'null'");
     this.chunkId = chunkId;
   }
 
   /**
-   * 
-   * @return
-   */
-  public ChunkRequest createChunkReq() {
-    return new ChunkRequest(getDestination(), getSource(), chunkId);
-  }
-
-  /**
+   * Returns a reference to the chunk identifier.
    *
-   * @return
+   * @return The advertised chunk unique identifier
    */
-  public ChunkId getChunkId() {
+  public PastryId getChunkId() {
     return chunkId;
   }
 
@@ -48,5 +49,15 @@ public class ChunkAdvertisement extends StarStreamMessage {
   @Override
   public Type getType() {
     return StarStreamMessage.Type.CHUNK_ADV;
+  }
+
+  /**
+   * Creates a {@link ChunkRequest} message used to show interest in obtaining
+   * the chunk from the advertising node.
+   *
+   * @return The {@link ChunkRequest} message
+   */
+  public ChunkRequest replyWithChunkReq() {
+    return new ChunkRequest(getDestination(), getSource(), chunkId);
   }
 }
