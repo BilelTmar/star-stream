2a91534548b889bf09b4c3e61a2ef125e0c68ef9
f.r.u.x.o.j <f.r.u.x.o.j@6e11d110-fbaa-11dd-87c9-111d11f6bf44>
2009-02-19 16:19:18 +0000

    Basic method renaming, comments and first impl. of the Chunk class.

--- trunk/src/com/google/code/peersim/starstream/protocol/StarStreamNode.java
+++ trunk/src/com/google/code/peersim/starstream/protocol/StarStreamNode.java
@@ -9,6 +9,8 @@ import com.google.code.peersim.pastry.protocol.PastryNode;
 import java.io.FileNotFoundException;
 
 /**
+ * This class represents a node that leverages both the *-Stream and the Pastry
+ * protocols to take part to a P2P streaming event.
  *
  * @author frusso
  * @version 0.1
@@ -16,6 +18,12 @@ import java.io.FileNotFoundException;
  */
 public class StarStreamNode extends PastryNode {
 
+  /**
+   * Default PeerSim-required constructor.
+   *
+   * @param prefix The prefix for accessing configuration properties
+   * @throws java.io.FileNotFoundException Raised whenever it is not possible creating the log file
+   */
   public StarStreamNode(String prefix) throws FileNotFoundException {
     super(prefix);
   }
