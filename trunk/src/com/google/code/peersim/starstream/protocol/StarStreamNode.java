4e4c284c58ce584d4c89e00150ff4b89c387c5f7
f.r.u.x.o.j <f.r.u.x.o.j@6e11d110-fbaa-11dd-87c9-111d11f6bf44>
2009-02-20 13:03:10 +0000

    Developments related to the *-Stream protocol: tied the *-Stream protocol to Pastry for protocol-events delivery. Fixed issues related to cloning. Added documentation.

--- trunk/src/com/google/code/peersim/starstream/protocol/StarStreamNode.java
+++ trunk/src/com/google/code/peersim/starstream/protocol/StarStreamNode.java
@@ -6,7 +6,9 @@
 package com.google.code.peersim.starstream.protocol;
 
 import com.google.code.peersim.pastry.protocol.PastryNode;
+import com.google.code.peersim.pastry.protocol.PastryProtocol;
 import java.io.FileNotFoundException;
+import peersim.config.Configuration;
 
 /**
  * This class represents a node that leverages both the *-Stream and the Pastry
@@ -21,6 +23,19 @@ import java.io.FileNotFoundException;
 public class StarStreamNode extends PastryNode {
 
   /**
+   * Configuration parameter key that ties a *-Stream node to its *-Stream protocol.
+   */
+  public static final String STAR_STREAM = "starstream";
+  /**
+   * The protocol identifier assigned to the *-Stream protocol by the PeerSim runtime.
+   */
+  private static int STAR_STREAM_PID;
+  /**
+   * Separator char used for PeersSim-related configuration properties.
+   */
+  private static final String SEPARATOR = ".";
+
+  /**
    * Default PeerSim-required constructor.
    *
    * @param prefix The prefix for accessing configuration properties
@@ -28,6 +43,12 @@ public class StarStreamNode extends PastryNode {
    */
   public StarStreamNode(String prefix) throws FileNotFoundException {
     super(prefix);
+    STAR_STREAM_PID = Configuration.getPid(prefix+SEPARATOR+STAR_STREAM);
+    // at this point the protocl stack for the current node must have been already
+    // initialized by the PeerSim runtime, so this is the right time for takeing
+    // references to Pastry and *-Stream and tight the two instance enabling the
+    // latter to receive notifications from the former
+    bindStarStreamToPastry();
   }
 
   /**
@@ -35,6 +56,26 @@ public class StarStreamNode extends PastryNode {
    */
   @Override
   public Object clone() {
-    return super.clone();
+    StarStreamNode clone = (StarStreamNode) super.clone();
+    clone.bindStarStreamToPastry();
+    return clone;
+  }
+
+  /**
+   * Returns a reference to this node's assigned {@link StarStreamProtocol} instance.
+   * @return The {@link StarStreamProtocol} instance
+   */
+  protected StarStreamProtocol getStarStreamProtocol() {
+    return (StarStreamProtocol) super.getProtocol(STAR_STREAM_PID);
+  }
+
+  /**
+   * This method has to be invoked both at construction and cloning-time to let
+   * the {@link StarStreamProtocol} instance register over the {@link PastryProtocol}
+   * instance for Pastry-related events.
+   */
+  private void bindStarStreamToPastry() {
+    PastryProtocol pastry = getPastryProtocol();
+    getStarStreamProtocol().registerPastryListeners(pastry);
   }
 }
